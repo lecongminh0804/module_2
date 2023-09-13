ed6702001d9f522b044bc8b6e93a30894318d720
Your Name <you@example.com>
2023-09-13 21:19:08 +0700

    up

---
+++ src/_01_introduction_java/exercise/Hienthiloichao.java
@@ -0,0 +1,10 @@
+package _01_introduction_java.exercise;
+import java.util.Scanner;
+public class Hienthiloichao {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter you name: " );
+        String name = Sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
--- src/_01_introduction_java/practice/E1.java
+++
@@ -1,7 +0,0 @@
-package _01_introduction_java.practice;
-
-public class E1 {
-    public static void main(String[] args) {
-        System.out.println("h");
-    }
-}
